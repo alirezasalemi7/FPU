7cf5098e8aebccc9a5dfd090c72bb4436c727d19
alireza salemi <alirezasalemi7@gmail.com>
2019-11-24 23:29:09 +0330

    project structure added

---
+++ dev/defs/header.h
@@ -0,0 +1,25 @@
+
+`define REG_SIZE 64
+`define S_MODE 0
+`define D_MODE 1
+`define NUM_OF_OPS 4
+`define OP_BITS 2
+`define INPUT_INTERFACE_INT_OUT 53
+`define INPUT_INTERFACE_EXP_OUT 12
+`define OP_DIV_S 2'b00
+`define OP_SQ_S 2'b01
+`define OP_DIV_D 2'b10
+`define OP_SQ_D 2'b11
+`define MODE_S_SIGN_BIT 31
+`define MODE_D_SIGN_BIT 63
+`define MODE_D_EXP_START 62
+`define MODE_D_EXP_END 52
+`define MODE_S_EXP_START 30
+`define MODE_S_EXP_END 23
+`define MODE_S_MAN_START 0
+`define MODE_S_MAN_END 22
+`define MODE_D_MAN_START 0
+`define MODE_D_MAN_END 51
+
+
+
